35c227c02dcf17bf12847d0fff4968bfd2a92c24
Louya64 <cixie@live.fr>
2021-04-18 16:04:52 +0200

    slick fini (sauf images)

--- v1/js/script.js
+++ v1/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     // $(".slick-prev").html("<i class=\"fas fa-chevron-left\"></i>");
     $('.accueil_sect3_slider').slick({
+        dots: true,
         lazyLoad: 'ondemand',
-        slidesToShow: 6,
+        slidesToShow: 5,
         slidesToScroll: 1
     });
+    $(".slick-dots li").html("•").css("color", "rgb(46, 167, 176)");
     $(".slick-prev").html("<i class=\"fas fa-chevron-left\"></i>");
     $(".slick-next").html("<i class=\"fas fa-chevron-right\"></i>");
   });
