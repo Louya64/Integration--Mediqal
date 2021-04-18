d0d34bd7f1a4dabe6ebc7b7db17483474fea72d4
Louya64 <cixie@live.fr>
2021-04-18 15:15:20 +0200

    slick-slider

--- v1/js/script.js
+++ v1/js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    $('.your-class').slick({
+    // $(".slick-prev").html("<i class=\"fas fa-chevron-left\"></i>");
+    $('.accueil_sect3_slider').slick({
         lazyLoad: 'ondemand',
-        slidesToShow: 5,
+        slidesToShow: 6,
         slidesToScroll: 1
     });
+    $(".slick-prev").html("<i class=\"fas fa-chevron-left\"></i>");
+    $(".slick-next").html("<i class=\"fas fa-chevron-right\"></i>");
   });
