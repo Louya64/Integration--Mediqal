457969825479b5a587dea56b55d63c5acab3d9dc
Louya64 <cixie@live.fr>
2021-04-20 20:08:20 +0200

    des détails sect4 et footer

--- v1/js/script.js
+++ v1/js/script.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
             $("nav").addClass("position-fixed");
             let translateY = (($(window).width()) * 250 / 928) - (992 * 250 / 928);
             $("main").css("transform", "translate(0, -" + translateY + "px)");
+            $("#accueil_sect4").addClass("container");
         } else {
             $("nav").removeClass("position-fixed");
             $("main").css("transform", "none");
+            $("#accueil_sect4").removeClass("container");
         }
     });
 
@@ -15,7 +17,33 @@ $(document).ready(function(){
         dots: true,
         lazyLoad: 'ondemand',
         slidesToShow: 5,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint:1500,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint:1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint:992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint:768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
     });
     $(".slick-dots li").html("•").css("color", "rgb(46, 167, 176)");
     $(".slick-prev").html("<i class=\"fas fa-chevron-left\"></i>");
