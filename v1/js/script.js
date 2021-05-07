a64c4436b71104343682f4b393345e62e0ae7669
Louya64 <cixie@live.fr>
2021-05-07 08:43:08 +0200

    modif responsive()

--- v1/js/script.js
+++ v1/js/script.js
@@ -1,13 +1,14 @@
+
 $(document).ready(function(){
 
     function responsive() {
         if (window.matchMedia("(min-width: 992px)").matches) {
             $("nav").addClass("position-fixed");
             let translateY = $("#carousel_accueil img").height() * 32 / 100;
-            // let translateY = ($(window).width() - 993) * 22 / 100;
             $("main").css("margin-top", "-" + translateY + "px");
             $("footer").css("margin-top", "-" + translateY + "px");
             $("#accueil_sect4").addClass("container");
+
             $(".footer_nav_children_p").css("display", "block");
             $(".footer_nav_children").unbind("click");
             $(window).unbind("click", footerEventResponsiveClear);
@@ -19,6 +20,8 @@ $(document).ready(function(){
             $("footer").css("margin-top", "0");
     
             $(".footer_nav_children_p").css("display", "none");
+            $(".footer_nav_children").css("z-index", "1");
+            $(".footer_nav_children").unbind("click");
             $(".footer_nav_children").bind("click", footerEventResponsive);
             $(window).bind("click", footerEventResponsiveClear);
         }
@@ -26,8 +29,9 @@ $(document).ready(function(){
     
     function footerEventResponsive(e) {
         let $p = $(this).find(".footer_nav_children_p");
-        let $this = $(this);
-        $this.css("z-index", "2");
+        $(".footer_nav_children_p").not($p).css("display", "none");
+        $(".footer_nav_children").not($(this)).css("z-index", "1");
+        $(this).css("z-index", "2");
         $p.toggle();
         e.stopPropagation();
     }
@@ -40,8 +44,7 @@ $(document).ready(function(){
     responsive();
 
     $(window).on("resize", function() {
-        // responsive();
-        location.reload();
+        responsive();
     });
 
     $('.accueil_sect3_slider').slick({
